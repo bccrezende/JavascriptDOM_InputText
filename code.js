b112af47ff33c2eb833b8d866c739dd78c2841d8
Barbara Rezende <bccrezende@gmail.com>
2022-05-20 08:45:51 -0300

    aula 01

--- code.js
+++ code.js
@@ -5,13 +5,14 @@ function clickButton(){
     arquivo = document.getElementById("inputfile");
 
     var fr=new FileReader();
+
     fr.readAsText(arquivo.files[0]);
 
     fr.onload=function(){
+        var info = fr.result; 
+        console.log(info);
         document.getElementById('output').textContent=fr.result;
-    }
-    
-    
+    }   
 }
 
 
