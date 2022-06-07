b2c66cb83013c2ab9226c55586706a2e283b7395
Barbara Rezende <bccrezende@gmail.com>
2022-06-07 17:02:21 -0300

    finalizacao do codigo

--- code.js
+++ code.js
@@ -1,7 +1,10 @@
-const inputButton = document.getElementById("inputfilebutton");
-inputButton.addEventListener("click", clickButton);
+const inputFileButton = document.getElementById("inputfilebutton");
+inputFileButton.addEventListener("click", clickFileButton);
 
-function clickButton(){
+const inputNumberButton = document.getElementById("inputnumberbutton");
+inputNumberButton.addEventListener("click", clickNumberButton);
+
+function clickFileButton(){
     arquivo = document.getElementById("inputfile");
 
     var fr=new FileReader();
@@ -13,9 +16,9 @@ function clickButton(){
             document.getElementById('output').textContent=fr.result;
         }  
     }
-    catch(error){
-        console.log(error);
-        document.getElementById('output').textContent="Arquivo não localizado";
+    catch(erro){
+        console.log(erro);
+        alert(erro);
     }
     finally{
         alert("Obrigado pela visita");
@@ -24,4 +27,28 @@ function clickButton(){
     
 }
 
+function clickNumberButton(){
+    // 1) Inserir um campo de input para entrada de dados - ok
+    // 2) Verifica se o dado de entrada é nulo, se for, lança exception "Informe um valor" - ok
+    // 3) Verifica se o dado de entrada é maior que 5 e menor que 10, se não for, lança exception 
+    // "Informe um valor maior que 5 e menor que 10"
+    // 4) Se o try falhar, lançar exception "Erro +  erro javascript"
+    // 5) No finnaly informar "O número escolhido foi + " numero
+    
+    document.getElementById("outputnumber").innerHTML = " ";
+    number = document.getElementById("inputnumber").value
+
+    try{
+        if (number == '') throw 'Informe um valor';
+        number = Number(number);
+        if (number <= 5 || number >= 10) throw 'Informe um valor maior que 5 e menor que 10'; 
+    }
+    catch(erro){
+        document.getElementById("outputnumber").innerHTML = erro;
+        console.log("Erro " + erro)
+    }
+    finally{
+        alert("O número escolhido foi: " + number);
+    }
+}
 
